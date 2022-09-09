7c184e2dfda0b51f49b325f270815fe49f39b49c
Lydia Zhang <lydiazhang@Lydias-MacBook-Pro.local>
2022-09-09 02:25:37 -0600

    simplified v

--- Scoreboard/ViewModel.swift
+++ Scoreboard/ViewModel.swift
@@ -10,17 +10,13 @@ import SwiftUI
 
 class ViewModel: ObservableObject {
     private(set) var viewIsColored = CurrentValueSubject<Bool, Never>(false)
-    
-    func countBump() { viewIsColored.value.toggle() }
 }
 
 class ViewModel2: ObservableObject {
     private(set) var isColored = CurrentValueSubject<Bool, Never>(false)
-    //private var subscriptions = Set<AnyCancellable>()
     @Published var vmColor: Color = .gray
     
     func bind() {
         self.vmColor = isColored.value ? .blue : .gray
-        //isColored.sink(receiveValue: { self.vmColor = ($0) ? .blue : .gray }).store(in: &subscriptions)
     }
 }
